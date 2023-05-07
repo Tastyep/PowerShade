ec3c17fbbc66374bf7c488395fab4578760b3483
Luc Sinet <luc.sinet@gmail.com>
2023-05-07 18:41:34 +0200

    feat: add style support for the Get-ChildItem command

--- color.ps1
+++ color.ps1
@@ -23,12 +23,21 @@ $commandToColors = @{
         '\".*\"'                                                      = NewAnsiSpec('Pale Green')  # '...'
     }
     'Get-ChildItem' = [ordered]@{
-        '^\bMode|\bLastWriteTime|\bLength\b|\bName\b' = NewAnsiSpec('Crimson') # header
-        '^d'                                          = NewAnsiSpec('Deep Sky Blue')
-        '^(?+)r(?+)\s'                                = NewAnsiSpec('Red')
-        '^.?(a)'                                      = NewAnsiSpec('Yellow')
-        '\s*(Directory:\s.*)'                         = NewAnsiSpec -Color 'Orange' -Style 'underline'
+        '\s*(Directory:\s.*)'                         = NewAnsiSpec -Color 'Orange' -Style 'underline' # Header
         '\w+$'                                        = NewAnsiSpec -Color 'Orange' -Style 'underline'
+        '^\bMode|\bLastWriteTime|\bLength\b|\bName\b' = NewAnsiSpec('Crimson') # SubHeader
+        '^d'                                          = NewAnsiSpec('Royal Blue') # Directory
+        '^d.*\s+\b(.*)\b'                             = NewAnsiSpec('Royal Blue')
+        '^.(a)'                                       = NewAnsiSpec('Khaki') # Archive
+        '^.(a)----.*\s+\b(.*)\b'                      = NewAnsiSpec('Khaki')
+        '^.{2}(r)'                                    = NewAnsiSpec('Lime Green') # Read-Only
+        '^.{2}(r).*\s+\b(.*)\b'                       = NewAnsiSpec('Lime Green')
+        '^.{3}(h)'                                    = NewAnsiSpec('Dark Gray') # Hidden
+        '^.{3}(h).*\s+\b(.*)\b'                       = NewAnsiSpec('Dark Gray')
+        '^.{4}(s)'                                    = NewAnsiSpec('Red') # System
+        '^.{4}(s).*\s+\b(.*)\b'                       = NewAnsiSpec('Red')
+        '^.{5}(l)'                                    = NewAnsiSpec('Orange') # Link
+        '^.{5}(l).*\s+\b(.*)\b'                       = NewAnsiSpec('Orange')
     }
     'Write-Output'  = [ordered]@{
     }
