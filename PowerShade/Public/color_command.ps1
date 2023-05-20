486abfda9dc461b39a528399a0bdc64851647571
Luc Sinet <luc.sinet@gmail.com>
2023-05-20 17:48:41 +0200

    feat: improve performance by buffering output
    The Add-PowerShadeStyle now accepts an optional BufferSize parameter
    Default: 20
    It defines the number of line to buffer before calling Write-Host
    Benchmark:
     - gci -Recurse: 14s
     - ls -Recurse: Before: 45s, After: 18s

--- PowerShade/Public/color_command.ps1
+++ PowerShade/Public/color_command.ps1
@@ -2,15 +2,17 @@ function Add-PowerShadeStyle()
 {
   [CmdletBinding()]
   param(
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, Position = 0)]
     $AnsiStyler,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, Position = 1)]
     [hashtable]$CommandToSpec,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, Position = 2)]
     [string]$CommandName,
     [Parameter(ValueFromRemainingArguments = $true)]
-    $Params
+    $Params,
+    [int]$BufferSize = 20
   )
+
   $commandInfo = $(Get-Command -Name $CommandName)
   $command = $commandInfo.Name
   if ($commandInfo.ResolvedCommand)
@@ -25,10 +27,26 @@ function Add-PowerShadeStyle()
   } else
   {
     $regexStyleMap = $CommandToSpec[$command]
-        
+    $lines = [System.Text.StringBuilder]::new()
+    $lineCount = 0
     Invoke-Expression ($command + ' ' + $Params -join ' ') | Out-String -Stream | ForEach-Object -Process {
       $coloredLine = $AnsiStyler.ColorizeLine($_, $regexStyleMap)
-      Write-Host $coloredLine
+      
+      [void]($lines.Append($coloredLine))
+      $lineCount += 1
+      if ($lineCount -ge $BufferSize)
+      {
+        Write-Output $lines.ToString()
+        [void]($lines.Clear())
+        $lineCount = 0
+      } else
+      {
+        [void]($lines.Append("`r`n"))
+      }
     }
+  }
+  if ($lineCount -gt 0)
+  {
+    Write-Host $lines.ToString()
   }
 }
